f7155e8a135f4588c4e2c6a0ed9abe9f5f51c56e
mahyarahimi2442 <mahyarahimi855@gmail.com>
2025-10-06 10:52:27 +0330

    init projects

---
+++ src/main/java/ir/ac/weblogg/WebloggApplication.java
@@ -0,0 +1,13 @@
+package ir.ac.weblogg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebloggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebloggApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/ac/weblogg/WebloggApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.ac.weblogg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebloggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
